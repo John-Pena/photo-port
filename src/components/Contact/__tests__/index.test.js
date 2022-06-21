c899450b9af767c30ce6e939f17d4e42bd2611f8
John-Pena <jmp1495@gmail.com>
2022-06-20 21:44:47 -0500

    tested contact component and recieved all passes

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+  it('renders', () => {
+    render(<ContactForm />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<ContactForm />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+it('renders', () => {
+  const { getByTestId } = render(<ContactForm />);
+
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  expect(getByTestId('h1tag')).toHaveTextContent('Contact me');
+})
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 // __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
@@ -6,13 +7,27 @@ import Nav from '..';
 
 afterEach(cleanup);
 
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
+
 describe('Nav component', () => {
   it('renders', () => {
     render(<Nav />);
   });
 
   it('matches snapshot', () => {
-    const { asFragment } = render(<Nav />);
+    const { asFragment } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected} 
+    />);
 
     expect(asFragment()).toMatchSnapshot();
   });
@@ -20,15 +35,28 @@ describe('Nav component', () => {
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected} 
+    />);
 
+    // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
 })
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
-    const { getByTestId } = render(<Nav />);
+    const { getByTestId } = render(<Nav 
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
 
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
