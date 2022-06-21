5c6cb280d5b7f2554a099a4a60f1783144d6b98f
John-Pena <jmp1495@gmail.com>
2022-06-20 19:11:05 -0500

    Gallery component set up with useState, useEffects, hooks, and passing test for rendering PhotoList and Gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable testing-library/prefer-screen-queries */
 // __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
@@ -14,18 +13,18 @@ describe('Nav component', () => {
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-  const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav />);
 
-  expect(getByLabelText('camera')).toHaveTextContent('📸');
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
@@ -34,4 +33,5 @@ describe('links are visible', () => {
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
