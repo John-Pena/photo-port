3f808ca21f772c24f9c0b22fab208d235ae4f83b
John-Pena <jmp1495@gmail.com>
2022-06-24 21:53:11 -0500

    created test to confirm Modal works and fired on click event

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -120,24 +123,21 @@ const PhotoList = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter(photo => photo.category === category);
-  const [currentPhoto, setCurrentPhoto] = useState();
 
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i});
+    setCurrentPhoto({ ...image, index: i });
     setIsModalOpen(!isModalOpen);
   };
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
       {isModalOpen && (
-        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
       )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
